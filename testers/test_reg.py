43adcccfbacea71f76cd6f86824cec38a6578cfe
jrad9921 <107748738+jrad9921@users.noreply.github.com>
2025-03-14 18:07:26 +0100

    Update test_reg.py

--- testers/test_reg.py
+++ testers/test_reg.py
@@ -22,16 +22,17 @@ from sklearn.metrics import mean_absolute_error
 from scipy.stats import pearsonr
 import torch.nn as nn
 import torch.nn.functional as F
+
 #%%
 #Parameters
 cohort = 'ukb'
 img_size = 180
-tensor_dir = f'../../images/{cohort}/npy_{cohort}{img_size}'
 column_name = 'age'
 task = 'regression'
 subg = 'male'
 task_name = f'{column_name}'
 method_name = 'supervised'
+n_classes = 1
 model_name = 'dense'
 n_folds = 1
 num_epochs = 1000
@@ -41,83 +42,27 @@ batch_size = 4
 b_size = 4
 lr = 1e-05
 n_splits = 3
+
+# Set Paths
+tensor_dir = f'../../images/{cohort}/npy_{cohort}{img_size}'
 unique_name = f'{task_name}_e{num_epochs}_n{nrows}_b{batch_size}_lr{lr}_s{n_splits}_im{img_size}'
 csv_test = f'../data/{cohort}/test/demographics.csv'
 output_path = f'../scores/{cohort}/{model_name}/test/{unique_name}'
 model_path = f'../models/{model_name}/{column_name}_e{num_epochs}_n{nrows}_b{batch_size}_lr{lr}_s{n_splits}_im{img_size}'
 log_path = f'../logs/aurocs/{cohort}/{model_name}/test'
-n_classes = 1
+
+#Swin Parameters
 patch_size = [8, 8, 8]
 window_size = [16, 16, 16]
 num_heads = [3,6,12,24]
 depths = [2,2,2,2]
 feature_size = 96
+
+#Set Device
 dev = "cuda:1"
 torch.cuda.set_device(dev)
 seed = 42
-#%%
-class SFCN(nn.Module):
-    def __init__(self, channel_number=[32, 64, 128, 256, 256, 64], output_dim=1, dropout=True):
-        super(SFCN, self).__init__()
-        n_layer = len(channel_number)
-        self.feature_extractor = nn.Sequential()
-        for i in range(n_layer):
-            if i == 0:
-                in_channel = 1
-            else:
-                in_channel = channel_number[i-1]
-            out_channel = channel_number[i]
-            if i < n_layer-1:
-                self.feature_extractor.add_module('conv_%d' % i,
-                                                  self.conv_layer(in_channel,
-                                                                  out_channel,
-                                                                  maxpool=True,
-                                                                  kernel_size=3,
-                                                                  padding=1))
-            else:
-                self.feature_extractor.add_module('conv_%d' % i,
-                                                  self.conv_layer(in_channel,
-                                                                  out_channel,
-                                                                  maxpool=False,
-                                                                  kernel_size=1,
-                                                                  padding=0))
-        self.classifier = nn.Sequential()
-        avg_shape = [5, 5, 5]
-        self.classifier.add_module('average_pool', nn.AvgPool3d(avg_shape))
-        if dropout is True:
-            self.classifier.add_module('dropout', nn.Dropout(0.5))
-        i = n_layer
-        in_channel = channel_number[-1]
-        out_channel = output_dim
-        self.classifier.add_module('conv_%d' % i,
-                                   nn.Conv3d(in_channel, out_channel, padding=0, kernel_size=1))
 
-    @staticmethod
-    def conv_layer(in_channel, out_channel, maxpool=True, kernel_size=3, padding=0, maxpool_stride=2):
-        if maxpool is True:
-            layer = nn.Sequential(
-                nn.Conv3d(in_channel, out_channel, padding=padding, kernel_size=kernel_size),
-                nn.BatchNorm3d(out_channel),
-                nn.MaxPool3d(2, stride=maxpool_stride),
-                nn.ReLU(),
-            )
-        else:
-            layer = nn.Sequential(
-                nn.Conv3d(in_channel, out_channel, padding=padding, kernel_size=kernel_size),
-                nn.BatchNorm3d(out_channel),
-                nn.ReLU()
-            )
-        return layer
-
-    def forward(self, x):
-        #out = list()
-        x_f = self.feature_extractor(x)
-        x = self.classifier(x_f)
-        #x = F.log_softmax(x, dim=1)
-        #print(x.shape)
-        x = x.mean(dim = [2,3,4])
-        x = x.squeeze()
-        return x
 # %%  
 auroc_list = [] 
 for fold in range(1, n_folds + 1):
